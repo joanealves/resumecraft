a3590f43f8fc58ffa2daedccdc40825c63b0bbd4
Joane <joanealves2011@gmail.com>
2025-01-04 18:58:10 -0300

    modal criação

--- src/app/dashboard/resumes/page.tsx
+++ src/app/dashboard/resumes/page.tsx
@@ -6,6 +6,7 @@ export default function DashboardResumesPage() {
   return (
     <>
       <h1 className="text-4xl font-title font-bold mb-6">Currículos</h1>
+      <ResumesList />
 
       <Suspense fallback={<ResumesListSkeleton />}>
         <ResumesList />
